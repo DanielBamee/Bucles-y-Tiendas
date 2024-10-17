ad6814fe8f3196e7bd16f64a8fc1354f05be2f6e
DanielBamee <danielbamee@gmail.com>
2024-10-17 12:13:22 +0100

    Botones
    Progreso en botones.
    Código terminado, falta arrastrar cada cosa a su lugar.

---
+++ Assets/Script/Wallet.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+    
+public class Wallet : MonoBehaviour
+{
+    float saldo;
+    [SerializeField]
+    TextMeshProUGUI labelSaldo;
+    [SerializeField]
+    GameObject confirmarCompra;
+    [SerializeField]
+    GameObject noSaldo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        saldo = Random.Range(450f, 950);
+        labelSaldo.text = saldo.ToString("000.00") + "$";
+    }
+
+    public void RestarSaldo(float precio)
+    {
+        if (saldo > precio)
+        {
+            saldo -= precio;
+            labelSaldo.text = saldo.ToString("000:00") + " $";
+        }
+        else
+        {
+            noSaldo.SetActive(true);
+        }
+    }
+}
